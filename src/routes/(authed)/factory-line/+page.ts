b65a3189ca4afda8f64abe99d35e400b831353d2
CookieHCl <nhc7502@snu.ac.kr>
2024-12-05 16:53:34 +0900

    Remove unnecessary page.ts files

--- src/routes/(authed)/factory-line/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-export const load: PageLoad = async ({ data }) => {
-    return {
-        userId: data.userId,
-        factoryLines: data.factoryLines
-    };
-};
