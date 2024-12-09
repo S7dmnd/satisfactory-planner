b36f674f7a58fd6d0418e0950d605f6de906b061
JunJin1218 <jin_jun@mymail.sutd.edu.sg>
2024-12-09 21:46:37 +0800

    Updated Loading Animation (root/layout.svelte에 넣음)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	import { onMount } from 'svelte';
+	import Loading from '$lib/components/Loading.svelte';
+
+	let ready = $state(false);
+	onMount(() => {
+		ready = true;
+	});
 </script>
 
+{#if !ready}
+	<Loading message="Loading..." />
+{/if}
 {@render children()}
