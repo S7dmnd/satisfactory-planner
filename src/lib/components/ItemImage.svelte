81e15d9f48fdf6d5b0beb735b55e50a24b1f3898
CookieHCl <nhc7502@snu.ac.kr>
2024-12-14 22:41:17 +0900

    Rename images into key instead of korean name

--- src/lib/components/ItemImage.svelte
+++ src/lib/components/ItemImage.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import itemNameMapping from '$lib/itemNameMapping.json';
+
 	let { itemName, style } = $props();
 </script>
 
-<img src="/src/lib/images/{itemName}.webp" alt={itemName} {style} />
+<img src="/src/lib/images/{itemNameMapping[itemName]}.webp" alt={itemName} {style} />
 
 <style>
 	img {
